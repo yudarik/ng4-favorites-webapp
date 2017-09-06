24bf833304b61594b50ab35a9addb4f56277064d
Arik Yudin <yudarik@gmail.com>
2017-09-06 11:54:29 +0300

    fix update modal bugs

--- src/app/remove-modal/remove-modal.component.ts
+++ src/app/remove-modal/remove-modal.component.ts
@@ -3,20 +3,7 @@ import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-remove-modal',
-  template: `<div class="modal-header">
-                  <h4 class="modal-title">Remove favorite website?</h4>
-                  <button type="button" class="close" aria-label="Close" (click)="d('Cross click')">
-                      <span aria-hidden="true">&times;</span>
-                  </button>
-              </div>
-              <div class="modal-body">
-                  <p>This action will remove the favorite website permanently.</p>
-              </div>
-              <div class="modal-footer">
-                  <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('approve')">Remove website</button>
-                  <button type="button" class="btn btn-outline-dark" (click)="activeModal.dismiss('dismiss')">Cancel</button>
-              </div>
-              `,
+  templateUrl: './remove-modal.component.html',
   styleUrls: ['./remove-modal.component.scss']
 })
 export class RemoveModalComponent implements OnInit {
